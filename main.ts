a7e8eddbaae0b852bbc84f0d0aa7cc2ce0d77af3
Shortbrocilli <122543889+Shortbrocilli@users.noreply.github.com>
2023-01-30 09:49:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+function updateScores () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player 1:" + p1)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player 2:" + p2)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties:" + ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds:" + rounds)
+}
+let ties = 0
+let p2 = 0
+let p1 = 0
+let rounds = 0
+OLED.init(128, 64)
+OLED.writeStringNewLine("RPS counter initializing")
+rounds = 0
+p1 = 0
+p2 = 0
+ties = 0
+basic.pause(2000)
+updateScores()
