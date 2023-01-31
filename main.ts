5b5bb022fadc0a12f41d16e325356054c07d5c1d
Shortbrocilli <122543889+Shortbrocilli@users.noreply.github.com>
2023-01-31 12:50:43 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,8 +8,18 @@ input.onButtonPressed(Button.B, function () {
 	
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    Reset()
 })
+function Reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("RPS counter initializing")
+    rounds = 0
+    p1 = 0
+    p2 = 0
+    ties = 0
+    basic.pause(2000)
+    updateScores()
+}
 function updateScores () {
     OLED.clear()
     OLED.writeStringNewLine("Player 1:" + p1)
@@ -24,11 +34,4 @@ let ties = 0
 let p2 = 0
 let p1 = 0
 let rounds = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("RPS counter initializing")
-rounds = 0
-p1 = 0
-p2 = 0
-ties = 0
-basic.pause(2000)
-updateScores()
+Reset()
